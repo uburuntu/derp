abbb6c9f8c727b60d94affabc293b3c849ca41fd
Ramzan Bekbulatov <uburuntu@users.noreply.github.com>
2025-06-02 21:13:35 +0100

    enh: support mentions in media caption's

--- derp/filters/derp_mention.py
+++ derp/filters/derp_mention.py
@@ -10,9 +10,10 @@ class DerpMentionFilter(BaseFilter):
     """Filter for messages that mention 'derp' or 'дерп'."""
 
     async def __call__(self, message: Message) -> bool:
-        if not message.text:
+        text = message.text or message.caption
+        if not text:
             return False
 
         # Case-insensitive check for "derp" in English or Russian
         pattern = r"\b(derp|дерп)\b"
-        return bool(re.search(pattern, message.text, re.IGNORECASE))
+        return bool(re.search(pattern, text, re.IGNORECASE))
